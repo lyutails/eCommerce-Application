a160bf4adf7919ee0be63a41eae0c8797d33db59
yanabel1996 <119679434+yanabel1996@users.noreply.github.com>
2023-08-03 13:03:10 +0200

    test: test husky

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,9 +1,8 @@
-import App from "./App";
-import { render, screen } from '@testing-library/react';
+import App from './App'
+import { render, screen } from '@testing-library/react'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeUndefined();
-});
-
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeUndefined()
+})
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,10 @@
-import './_app.scss';
-
+import './_app.scss'
 
 function App() {
-
   return (
     <div>
       <h1 className="app-title">Hello!</h1>
     </div>
-  );
-};
-export default App;
+  )
+}
+export default App
