7c41b18e9c3550f7ff454c730e85656f8e1fbcb8
yanabel1996 <119679434+yanabel1996@users.noreply.github.com>
2023-08-10 19:21:39 +0200

    feat: start to verify email input

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,38 +1,23 @@
 // import { tokenCustomer } from '../../src/api/clientBuilder';
 // import { tokenAdmin } from '../../src/api/adminBuilder';
 // import { apiRoot } from '../../src/api/createClientAdmin';
-import { getCategories } from '../api/createClient';
-// import style from './_app.module.scss';
-// import AuthPage from '../pages/Auth/Auth';
+import { checkEmail } from '../pages/Auth/verify';
+// import { getCategories } from '../api/createClient';
+import style from './_app.module.scss';
+import AuthPage from '../pages/Auth/Auth';
 import RegistrationPage from '../pages/Registration/Registration';
 // import Input from '../components/Input/Input';
 // import { email, password } from '../components/Input/input-data';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { setImage } from '../store/counterSlice';
+import { Link, useNavigate } from 'react-router-dom';
+import React, { ChangeEvent, MouseEventHandler, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function App(): JSX.Element {
-  // console.log(apiRoot.products().get());
-  // console.log(getAllProducts);
-  console.log(getCategories());
-  console.log(process.env.ADMIN_CLIENT_ID, 'lalala');
-  // const dispatch = useDispatch();
-  // const url = getAllProducts(dispatch(setImage));
-  // const image = useSelector(state => state.);
-  /* console.log(
-    url.then((response) => {
-      return response.body.results[0].masterData.current.variants[0].images;
-    })
-  ); */
-  // url?.length ? console.log(url[0].url) : console.log('error');
-  // console.log(getAllCustomers());
-  // console.log(apiRoot.productProjections().get());
-  // console.log(apiRoot.customers().get());
-  // console.log(apiRoot.categories().get());
-  // console.log(tokenAdmin);
-  // console.log(tokenCustomer);
   return (
     <div>
-      <RegistrationPage />
+      <AuthPage />
     </div>
   );
 }
--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -4,6 +4,8 @@ import ButtonForm from '../../components/shared/ButtonForm/Button';
 import iconEmail from '../../../public/assets/icon/email.svg';
 import iconPassword from '../../../public/assets/icon/password.svg';
 import iconEye from '../../../public/assets/icon/eye.svg';
+// import { checkEmail } from '../Auth/verify';
+// import { ChangeEventHandler } from 'react';
 
 const clue = {
   invalidEmail:
@@ -22,68 +24,140 @@ const clue = {
 function AuthPage(): JSX.Element {
   return (
     <div className={style.login}>
-      <AuthPageLogin />
-      <AuthPageRegister />
-    </div>
-  );
-}
-
-function AuthPageLogin(): JSX.Element {
-  return (
-    <div className={style.authorization}>
-      <h2 className={style.title}>Login</h2>
-      <form action="" className={style.authorization_form}>
-        <Input
-          type="email"
-          placeholder="E-mail"
-          classWrapper={style.email}
-          classClue={style.email_clue}
-          classInput={style.email_input}
-          childrenBefore={
-            <div className={style.wrapper_img}>
-              <img
-                className={style.wrapper_img_icon}
-                src={iconEmail}
-                alt="Icon"
-              />
-            </div>
-          }
-        />
-        <Input
-          type="password"
-          placeholder="Password"
-          classWrapper={style.password}
-          classClue={style.password_clue}
-          classInput={style.password_input}
-          childrenBefore={
-            <div className={style.wrapper_img}>
-              <img
-                className={style.wrapper_img_icon}
-                src={iconPassword}
-                alt="Icon"
-              />
-            </div>
-          }
-          childrenAfter={
-            <ButtonForm classNames={style.password_eye}>
-              <img className={style.label_img_icon} src={iconEye} alt="Icon" />
-            </ButtonForm>
-          }
-        />
-        <ButtonForm classNames={style.authorization_button} type="submit">
-          LogIn
-        </ButtonForm>
-      </form>
-    </div>
-  );
-}
-
-function AuthPageRegister(): JSX.Element {
-  return (
-    <div className={style.registration}>
-      <h2 className={style.title}>Registartion</h2>
-      <ButtonForm classNames={style.registration_button}>SignUp</ButtonForm>
+      <div className={style.authorization}>
+        <h2 className={style.title}>Login</h2>
+        <form action="" className={style.authorization_form}>
+          <Input
+            // func={(event: ChangeEventHandler<HTMLInputElement>): void =>
+            //   checkEmail(event)
+            // }
+            type="email"
+            placeholder="E-mail"
+            classWrapper={style.email}
+            classClue={style.email_clue}
+            classInput={style.email_input}
+            childrenBefore={
+              <div className={style.wrapper_img}>
+                <img
+                  className={style.wrapper_img_icon}
+                  src={iconEmail}
+                  alt="Icon"
+                />
+              </div>
+            }
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            classWrapper={style.password}
+            classClue={style.password_clue}
+            classInput={style.password_input}
+            childrenBefore={
+              <div className={style.wrapper_img}>
+                <img
+                  className={style.wrapper_img_icon}
+                  src={iconPassword}
+                  alt="Icon"
+                />
+              </div>
+            }
+            childrenAfter={
+              <ButtonForm classNames={style.password_eye}>
+                <img
+                  className={style.label_img_icon}
+                  src={iconEye}
+                  alt="Icon"
+                />
+              </ButtonForm>
+            }
+          />
+          <ButtonForm classNames={style.authorization_button} type="submit">
+            LogIn
+          </ButtonForm>
+        </form>
+      </div>
+      <div className={style.registration}>
+        <h2 className={style.title}>Registartion</h2>
+        <ButtonForm classNames={style.registration_button}>SignUp</ButtonForm>
+      </div>
     </div>
   );
 }
 export default AuthPage;
+
+//  // const dispatch = useDispatch();
+//  const [login, setLogin] = useState('');
+//  const [password, setPassword] = useState('');
+//  //состояние ошибки
+//  const [loginError, setLoginError] = useState('');
+//  const [passwordError, setPasswordError] = useState('');
+//  //для навигации
+//  // const navigate = useNavigate();
+//  // для проверки состояний
+//  let loginСheck = false;
+//  let passwordСheck = false;
+
+//  // чтобы значения из инпута попадали в логин и пароль
+//  const loginHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+//    setLogin((e.target as HTMLInputElement).value);
+//  };
+
+//  const passwordHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+//    setPassword((e.target as HTMLInputElement).value);
+//  };
+
+//  const handeleСreationReg = (
+//    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+//  ): void => {
+//    e.preventDefault();
+//    //валидация для логина
+//    if (login === '') {
+//      setLoginError('Поле не должно быть пустым');
+//      loginСheck = false;
+//    } else if (login.length < 4) {
+//      setLoginError('Логин должен содержать не менее 4-х символов');
+//      loginСheck = false;
+//    } else {
+//      setLoginError('');
+//      loginСheck = true;
+//    }
+//    //валидация для пароля
+//    if (password === '') {
+//      setPasswordError('Поле не должно быть пустым');
+//      passwordСheck = false;
+//    } else if (password.length < 4) {
+//      setPasswordError('Логин должен содержать не менее 4-х символов');
+//      passwordСheck = false;
+//    } else {
+//      setPasswordError('');
+//      passwordСheck = true;
+//    }
+//    console.log(loginСheck, passwordСheck);
+//    console.log(login, password);
+// проверка общего состояния и если все хорошо переход на Вход
+// if (loginСheck === true && passwordСheck === true) {
+//   dispatch(
+//     creationAuth({
+//       login,
+//       password,
+//     })
+//   );
+//   navigate('/');
+// }
+//  };
+//  return (
+//    <div>
+//      <form action="">
+//        <input type="email" onChange={(event): void => loginHandler(event)} />
+//        <span className={style.red}>{loginError}</span>
+//        <input
+//          type="password"
+//          onChange={(event): void => passwordHandler(event)}
+//        />
+//        <span className={style.red}>{passwordError}</span>
+//        <button onClick={(event): void => handeleСreationReg(event)}>
+//          Click
+//        </button>
+//      </form>
+//    </div>
+//  );
