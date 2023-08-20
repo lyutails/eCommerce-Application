64299049e22b746179293e25035dc5717c85c9a9
CRAFTSW0MAN <105918726+CRAFTSW0MAN@users.noreply.github.com>
2023-08-20 16:44:54 +0600

    fix: fix file main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
 import React from 'react';
-import App from './app/App';
+import App from './App/App';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
